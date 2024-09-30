1efc2fec758afc4ad66125765d94ea3198a98810
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-09-30 16:57:16 +0300

    implemented basic services

---
+++ authentication-service/src/test/java/com/ukma/authentication/service/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukma.authentication.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
