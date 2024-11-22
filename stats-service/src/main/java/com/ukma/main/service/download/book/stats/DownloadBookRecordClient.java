0e4339da138a81dbd7caeda0639692662e1c63e4
AnatoliyAndrus <anatoliyandrusenko72@gmail.com>
2024-11-22 03:53:45 +0200

    Added WireMock tests for stats-service

--- stats-service/src/main/java/com/ukma/main/service/download/book/stats/DownloadBookRecordClient.java
+++ stats-service/src/main/java/com/ukma/main/service/download/book/stats/DownloadBookRecordClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient("main-service")
+@FeignClient(value = "main-service", url = "http://localhost:8072")
 public interface DownloadBookRecordClient {
 
     @GetMapping("/api/download-records/{bookId}")
